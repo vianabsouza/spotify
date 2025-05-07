2c474022152509c04e38cdc797fd965c830c6179
Barbara Viana <vianabsouza@gmail.com>
2025-05-07 01:06:20 -0300

    chore: wip - start main component with basic structure and styles

---
+++ src/components/Main.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const Main = () => {
+  return (
+    <div className="main">
+      <div className="main__texts">
+        <h2>Artistas populares</h2>
+        <a className="main__link" href="/">
+          Mostrar tudo
+        </a>
+      </div>
+    </div>
+  );
+};
+
+export default Main;
