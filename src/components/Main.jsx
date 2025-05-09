72bd2f068d1afe9832d76ae47760a9d168b7ea8a
Barbara Viana <vianabsouza@gmail.com>
2025-05-09 11:13:36 -0300

    wip: add static layout for artists and songs in main and update css styles

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,13 +1,56 @@
 import React from "react";
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome"
+import { faCirclePlay } from "@fortawesome/free-solid-svg-icons";
+
 
 const Main = () => {
   return (
     <div className="main">
-      <div className="main__texts">
-        <h2>Artistas populares</h2>
-        <a className="main__link" href="/">
-          Mostrar tudo
-        </a>
+      <div className="item-list">
+        <div className="item-list__header">
+          <h2>Artistas populares</h2>
+          <a className="item-list__link" href="/">
+            Mostrar tudo
+          </a>
+        </div>
+        <div className="item-list__container">
+          <div className="single-item">
+            <div className="single-item__div-image-button">
+              <div className="single-item__div-image">
+                <img className="single-item__image" src="https://i.scdn.co/image/ab676161000051744dcd8a3bff84cd7703892cf4" alt="Imagem do Artista Henrique & Juliano" />
+              </div>
+              <FontAwesomeIcon className="single-item__icon" icon={faCirclePlay}/>
+            </div>
+            <div className="single-item__texts">
+              <p className="single-item__title">Henrique & Juliano</p>
+              <p className="single-item__type">Artista</p>
+            </div>
+          </div>
+        </div>
+      </div>
+
+
+      <div className="item-list">
+        <div className="item-list__header">
+          <h2>Músicas populares</h2>
+          <a className="item-list__link" href="/">
+            Mostrar tudo
+          </a>
+        </div>
+        <div className="item-list__container">
+          <div className="single-item">
+            <div className="single-item__div-image-button">
+              <div className="single-item__div-image">
+                <img className="single-item__image" src="https://i.scdn.co/image/ab676161000051744dcd8a3bff84cd7703892cf4" alt="Imagem do Artista Henrique & Juliano" />
+              </div>
+              <FontAwesomeIcon className="single-item__icon" icon={faCirclePlay}/>
+            </div>
+            <div className="single-item__texts">
+              <p className="single-item__title">Henrique & Juliano</p>
+              <p className="single-item__type">Artista</p>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
